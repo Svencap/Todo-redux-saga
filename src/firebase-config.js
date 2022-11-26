adf28e47bab1a4caa5ccc6d9d0a0a8415c4a11f7
Ayzek <Ayzekmanduar@mail.ru>
2022-11-27 05:47:02 +0900

    add design

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABuyMFMtWy5hgug0Jdbt_Mi3KSgPFo00I",
+  authDomain: "todo-redux-saga-b7260.firebaseapp.com",
+  projectId: "todo-redux-saga-b7260",
+  storageBucket: "todo-redux-saga-b7260.appspot.com",
+  messagingSenderId: "888109302983",
+  appId: "1:888109302983:web:45ed2527b5346795a32ae2",
+  measurementId: "G-9D9WEV82YM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
