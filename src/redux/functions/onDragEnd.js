d97317a09f83c547423d7359d4dca0e5c2523b5b
Ayzek <Ayzekmanduar@mail.ru>
2022-12-02 06:16:49 +0900

    fix DnD

--- src/redux/functions/onDragEnd.js
+++ src/redux/functions/onDragEnd.js
@@ -4,38 +4,39 @@ const onDragEnd = (tasks, result, startIndex, endIndex, dispatch, actionSortedTa
   if (!result.destination) return;
 
   if (result.destination.droppableId === "development") {
-    const updateTasks = [...tasks].map(({ id, title, description, status, subTasks, expirationDate, files }) => {
+    const updateTasks = [...tasks].map(({ id, title, description, priority, createdDate, status, subTasks, expirationDate, files }) => {
       if (id === result.draggableId) {
         const newStatus = 'Development';
         const checkFiles = files ? files : [];
-        return { id, title, description, status: newStatus, expirationDate, subTasks, files: checkFiles };
+        return { id, title, description, status: newStatus, priority, createdDate, expirationDate, subTasks, files: checkFiles };
       }
-      return { id, title, description, status, subTasks, expirationDate, files };
+      return { id, title, description, status, subTasks, priority, createdDate, expirationDate, files };
     });
     dispatch(actionChangeDev({id: result.draggableId, status: 'Development', updateTasks}));
   }
 
   if (result.destination.droppableId === "to_do") {
-    const updateTasks = [...tasks].map(({ id, title, description, status, subTasks, expirationDate, files }) => {
+    const updateTasks = [...tasks].map(({ id, title, description, status, priority, createdDate, subTasks, expirationDate, files }) => {
       if (id === result.draggableId) {
         const newStatus = 'Queue';
         const checkFiles = files ? files : [];
-        return { id, title, description, status: newStatus, expirationDate, subTasks, files: checkFiles };
+        return { id, title, description, status: newStatus, priority, createdDate, expirationDate, subTasks, files: checkFiles };
       }
-      return { id, title, description, status, subTasks, expirationDate, files };
+      return { id, title, description, status, subTasks, priority, createdDate, expirationDate, files };
     });
     dispatch(actionChangeDev({id: result.draggableId, status: 'Queue', updateTasks}));
   }
 
   if (result.destination.droppableId === "Done") {
-    const updateTasks = [...tasks].map(({ id, title, description, status, subTasks, expirationDate, files }) => {
+    const updateTasks = [...tasks].map(({ id, title, description, status, priority, createdDate, subTasks, expirationDate, files }) => {
       if (id === result.draggableId) {
         const newStatus = 'Done';
         const checkFiles = files ? files : [];
-        return { id, title, description, status: newStatus, expirationDate, subTasks, files: checkFiles };
+        return { id, title, description, status: newStatus, priority, createdDate, expirationDate, subTasks, files: checkFiles };
       }
-      return { id, title, description, status, subTasks, expirationDate, files };
+      return { id, title, description, status, subTasks, priority, createdDate, expirationDate, files };
     });
+    console.log(updateTasks);
     dispatch(actionChangeDev({id: result.draggableId, status: 'Done', updateTasks}));
   }
 
