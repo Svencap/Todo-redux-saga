e53c38dff50b119e1ce7ea0068bb96626f2270a1
Ayzek <Ayzekmanduar@mail.ru>
2022-12-04 06:13:29 +0900

    add new actions

--- src/redux/actions/actionCreator.js
+++ src/redux/actions/actionCreator.js
@@ -1,8 +1,26 @@
-import { GET_TASKS, SET_TASKS, ADD_TASK, UPLOAD_FILES, SORTED_TASKS, CHANGE_STATUS_DEVELOPMENT } from "../contants";
+import {
+  GET_TASKS,
+  SET_TASKS,
+  ADD_TASK,
+  UPLOAD_FILES,
+  SORTED_TASKS,
+  CHANGE_STATUS_DEVELOPMENT,
+  DELETE_TASK,
+  DELETE_SUBTASK,
+  DELETE_FILE,
+} from "../contants";
 
 export const getTasks = () => ({ type: GET_TASKS });
 export const setTasks = (payload) => ({ type: SET_TASKS, payload });
 export const addTask = (payload) => ({ type: ADD_TASK, payload });
-export const uploadFiles = (payload) => ({type: UPLOAD_FILES, payload }); 
-export const sortedTaks = (payload) => ({type: SORTED_TASKS, payload });
-export const changeStatusDevelopment = (payload) => ({type: CHANGE_STATUS_DEVELOPMENT, payload });
+export const uploadFiles = (payload) => ({ type: UPLOAD_FILES, payload });
+export const sortedTaks = (payload) => ({ type: SORTED_TASKS, payload });
+
+export const changeStatusDevelopment = (payload) => ({
+  type: CHANGE_STATUS_DEVELOPMENT,
+  payload,
+});
+
+export const deleteTask = (payload) => ({ type: DELETE_TASK, payload });
+export const deleteSubTask = (payload) => ({ type: DELETE_SUBTASK, payload });
+export const deleteFile = (payload) => ({ type: DELETE_FILE, payload });
