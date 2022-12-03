75e8e3e2ee67d20b3cc7af9c2a3f9087d9be0361
Ayzek <Ayzekmanduar@mail.ru>
2022-12-04 06:07:12 +0900

    add Modal for delete Task

--- src/components/Modals/DeleteTask.jsx
+++ src/components/Modals/DeleteTask.jsx
@@ -1,17 +1,42 @@
-import '../../css/DeleteTaskModal.css';
+import { useDispatch } from "react-redux";
+import { deleteTask } from "../../redux/actions/actionCreator";
 
+import "../../css/DeleteTaskModal.css";
 
-const DeleteTask = ({ active, setActive }) => {
+const DeleteTask = ({ id, files, setActive }) => {
+  const dispatch = useDispatch();
   return (
     <div className="delete_wrapper">
       <div className="delete_modal_header">
-        <span>Удалить</span>
-        <button onClick={() => setActive(false)}>Х</button>
+        <span>Удалить задачу</span>
+        <button className="close_delete_modal" onClick={() => setActive(false)}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 5L5 19M19 19L5 5"
+              stroke="black"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+          </svg>
+        </button>
       </div>
-      <div className="delete_modal_body">Вы не сможете восстановить задачу</div>
+      <div className="delete_modal_body">Вы действительно хотите удалить?</div>
       <div className="delete_modal_buttons">
-        <button onClick={() => setActive(false)} className='close'>Закрыть</button>
-        <button onClick={() => setActive(false)} className='delete'>Удалить</button>
+        <button onClick={() => setActive(false)} className="close">
+          Закрыть
+        </button>
+        <button onClick={() => {
+          dispatch(deleteTask({ id, files }));
+          // setActive(false);
+        }} className="delete">
+          Удалить
+        </button>
       </div>
     </div>
   );
