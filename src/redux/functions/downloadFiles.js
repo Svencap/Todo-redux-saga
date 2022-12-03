cb221ba1156f7c9f89d3d6f8636dc9a35d4a358c
Ayzek <Ayzekmanduar@mail.ru>
2022-12-04 06:14:53 +0900

    fix function

--- src/redux/functions/downloadFiles.js
+++ src/redux/functions/downloadFiles.js
@@ -1,11 +1,11 @@
 import uploadFilesToStorage from "./uploadFilesToStorage";
 
-const downloadFiles = async (files) => {
+const downloadFiles = async (taskId, files) => {
   return Promise.all(files.map(async ({ id, name, selectedFile, url }) => {
     const getName = name ? name : selectedFile.name;
     const getId = `${id}_${getName}`;
     const getUrl = url ? url : await uploadFilesToStorage(getId, selectedFile);
-    return { id: getId, name: getName, url: getUrl };
+    return { taskId, id: getId, name: getName, url: getUrl };
   }))
 };
 
